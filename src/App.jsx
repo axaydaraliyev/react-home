ea8eff3f312922fbd47dda4db5636cc61093bbc5
a <a>
2025-12-08 13:21:21 +0500

    half

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
